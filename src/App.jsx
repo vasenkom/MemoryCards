471a9e46e6420238d976b27f356a742fb42d6298
vasenkom <m.vasenko@yandex.ru>
2024-10-08 20:39:00 +0200

    add fucntions to change to Random Cat Fact, memorisation game or Info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,90 @@
 // App.js
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Header } from "./Header.jsx";
+import { HomeRectangle } from "./HomeRectangle.jsx";
 import "./App.css";
 
 function App() {
   // State to track the current content of the rectangle
   const [content, setContent] = useState(null);
 
-  // Functions to handle button clicks
-  const showMemoRiseInfo = () => {
-    setContent(
+  function FAQ_MemoRise() {
+    return (
       <div>
-        <h2>About MemoRise</h2>
         <p>
-          MemoRise is a fun and engaging way to improve your memorization
-          skills. With various exercises and games, you can enhance your memory
-          retention.
+          MemoRise is a mini project made by{" "}
+          <span className="green">vasenkom</span>.
         </p>
-      </div>
-    );
-  };
-
-  const showMemoryGame = () => {
-    setContent(
-      <div>
-        <h2>Memory Game</h2>
+        <br />
         <p>
-          Welcome to the Memory Game! Try to match pairs of cards by remembering
-          their positions.
+          On this web page you can play a{" "}
+          <span className="purple">memorisation game</span> or read a{" "}
+          <span className="orange">random cat fact</span>.
         </p>
-        {/* You can include game logic or components here */}
+        <br />
+        <ul className="listOfOptions">
+          <li>How to play a memo game</li>
+          <li>
+            Check{" "}
+            <a className="green" href="https://www.example.com">
+              vasenkom
+            </a>{" "}
+            github
+          </li>
+          <button onClick={() => setContent(null)}>
+            Get back to main screen
+          </button>
+        </ul>
       </div>
     );
-  };
+  }
 
-  const showRandomCatFact = async () => {
-    const response = await fetch("https://meowfacts.herokuapp.com/");
-    const data = await response.json();
-    setContent(
+  function PlayGame() {
+    return (
       <div>
-        <h2>Random Cat Fact</h2>
-        <p>{data.data[0]}</p>
+        <p>In process</p>
       </div>
     );
-  };
+  }
+
+  function RandomCatFact() {
+    let [catFact, setCatFact] = useState("");
+
+    useEffect(() => {
+      fetch("https://meowfacts.herokuapp.com/")
+        .then((response) => response.json())
+        .then((data) => setCatFact(data.data[0]));
+    }, []);
+    return (
+      <div>
+        <p>{catFact}</p>
+        <button onClick={() => setContent(null)}>
+          Get back to main screen
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      <h1>Welcome to MemoRise</h1>
-      <p>This content does not change when you click the buttons below.</p>
+      <Header />
 
-      {/* Rectangle content */}
-      <div className="rectangle">
-        {content ? content : <p>Click a button to see more content.</p>}
-      </div>
+      <div className="main">
+        {/* Here will be some pics */}
 
-      {/* Buttons only shown when there's no content */}
-      {!content && (
-        <div className="buttons">
-          <button onClick={showMemoRiseInfo}>Know more about MemoRise</button>
-          <button onClick={showMemoryGame}>Play memorisation game</button>
-          <button onClick={showRandomCatFact}>Get a random cat fact</button>
+        <div className="rectangle">
+          {content ? (
+            content
+          ) : (
+            <HomeRectangle
+              setContent={setContent}
+              FAQ_MemoRise={FAQ_MemoRise}
+              PlayGame={PlayGame}
+              RandomCatFact={RandomCatFact}
+            />
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
