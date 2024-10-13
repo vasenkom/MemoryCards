0acff26c80ec908668c775560baa2b702b5e8e19
vasenkom <m.vasenko@yandex.ru>
2024-10-13 23:10:06 +0200

    fetch 5 beautiful painting from museum API and fix css to flex row wrap

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,30 @@ function App() {
   // State to track the current content of the rectangle
   const [content, setContent] = useState(null); // to display right component in .rectangle
   const [gameMode, setGameMode] = useState(false);
-  const [memoryCard, setMemoryCard] = useState([]);
+  const [memoryCard, setMemoryCards] = useState([]);
   const [loading, setLoading] = useState(false); // loading icon to wait until all images are loaded
 
   useEffect(() => {
     if (gameMode) {
-      fetch(
-        "https://collectionapi.metmuseum.org/public/collection/v1/objects/45734"
-      )
-        .then(setLoading(true))
-        .then((res) => res.json())
-        .then((data) => {
-          setContent(data);
+      setLoading(true);
+      const objectIds = [15051, 51660, 436163, 813095, 437423, 436947]; // IDs of 5 paintings with cats
+      const fetchData = async () => {
+        try {
+          const fetchedCards = await Promise.all(
+            objectIds.map((id) =>
+              fetch(
+                `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+              ).then((res) => res.json())
+            )
+          );
+          setMemoryCards(fetchedCards);
           setLoading(false);
-        })
-        .catch((error) => {
+        } catch (error) {
           console.error("Problem fetching data", error);
-        });
+          setLoading(false);
+        }
+      };
+      fetchData();
     }
   }, [gameMode]);
 
@@ -47,16 +54,18 @@ function App() {
           <LoadingScreen />
         ) : gameMode ? (
           <div className="cards">
-            <div className="card">
-              <img
-                className="cardImg"
-                src={content.primaryImage}
-                alt={content.title}
-              />
-              <p className="cardDescription">
-                {content.title}, {content.artistDisplayName}
-              </p>
-            </div>
+            {memoryCard.map((card, index) => (
+              <div className="card" key={index}>
+                <img
+                  className="cardImg"
+                  src={card.primaryImage}
+                  alt={card.title}
+                />
+                <p className="cardDescription">
+                  {card.title}, {card.artistDisplayName}
+                </p>
+              </div>
+            ))}
           </div>
         ) : content ? (
           content
