a92edd9ceb3340ee4e7b876739430e638adbfe22
vasenkom <m.vasenko@yandex.ru>
2024-10-14 18:20:15 +0200

    add new Star image to home screen

--- src/HomeRectangle.jsx
+++ src/HomeRectangle.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Star from "./assets/Star1.svg";
+import StarTalk from "./assets/StarTalk.svg";
+import StarTalkyTalk from "./assets/StarTalkyTalk.svg";
 
 export function HomeRectangle({
   setContent,
@@ -9,52 +11,54 @@ export function HomeRectangle({
   StartGame,
 }) {
   return (
-    <div className="rectangle" style={{ backgroundColor: "#f7d2b7" }}>
-      <p className="titleRectangle">
-        Hi! Welcome to <span className="blue"> MemoRise</span>!
-      </p>
-      <div className="columns">
-        <div className="column">
-          <img src={Star} alt="StarIcon" />
-        </div>
-        <div className="column">
-          <p>Would you like to:</p>
-          <div className="buttonsDIV">
-            <button
-              onClick={() =>
-                setContent(
-                  <FAQ_MemoRise
-                    RulesExplained={RulesExplained}
-                    setContent={setContent}
-                    StartGame={StartGame}
-                  />
-                )
-              }
-            >
-              1. Know more about <span className="blue">MemoRise</span>
-            </button>
-            <button
-              onClick={() =>
-                setContent(
-                  <RulesExplained
-                    setContent={setContent}
-                    StartGame={StartGame}
-                  />
-                )
-              }
-            >
-              2. Play <span className="purple">memorisation game</span>
-            </button>
-            <button
-              onClick={() =>
-                setContent(<RandomCatFact setContent={setContent} />)
-              }
-            >
-              3. Get a <span className="orange">random cat fact</span>
-            </button>
+    <>
+      <div className="rectangle" style={{ backgroundColor: "#f7d2b7" }}>
+        <p className="titleRectangle">
+          Hi! Welcome to <span className="blue"> MemoRise</span>!
+        </p>
+        <div className="columns">
+          <div className="column">
+            <img src={StarTalkyTalk} alt="StarIcon" />
+          </div>
+          <div className="column">
+            <p>Would you like to:</p>
+            <div className="buttonsDIV">
+              <button
+                onClick={() =>
+                  setContent(
+                    <FAQ_MemoRise
+                      RulesExplained={RulesExplained}
+                      setContent={setContent}
+                      StartGame={StartGame}
+                    />
+                  )
+                }
+              >
+                1. Know more about <span className="blue">MemoRise</span>
+              </button>
+              <button
+                onClick={() =>
+                  setContent(
+                    <RulesExplained
+                      setContent={setContent}
+                      StartGame={StartGame}
+                    />
+                  )
+                }
+              >
+                2. Play <span className="purple">memorisation game</span>
+              </button>
+              <button
+                onClick={() =>
+                  setContent(<RandomCatFact setContent={setContent} />)
+                }
+              >
+                3. Get a <span className="orange">random cat fact</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
